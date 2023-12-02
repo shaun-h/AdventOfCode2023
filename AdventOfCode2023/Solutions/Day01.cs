6742f7ae8fc883ec7da9b65ae82d066096d641a1
Shaun Hevey <shaun.hevey@gmail.com>
2023-12-03 08:55:47 +1100

    Day 01 Part 02

--- AdventOfCode2023/Solutions/Day01.cs
+++ AdventOfCode2023/Solutions/Day01.cs
@@ -2,23 +2,43 @@ namespace AdventOfCode2023.Solutions;
 
 public class Day01
 {
-    public static int Solve(string input)
+    public static int Solve(string input, bool parseWords)
     {
         var lines = input.Split(Environment.NewLine);
 
-        var lineNumbers = lines.Select(ProcessLine);
+        var lineNumbers = lines.Select(line => ProcessLine(line, parseWords)).Where(number => number > -1);
         return lineNumbers.Aggregate(0, (i1, i2) => i1 + i2);
     }
 
-    private static int ProcessLine(string line)
+    private static int ProcessLine(string line, bool parseWords)
     {
+        var words = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
         var numbersFound = new List<int>();
-        foreach (var c in line)
+        for(var i = 0; i < line.Length; i++)
         {
-            if (int.TryParse(c.ToString(), out var i))
+            var c = line[i];
+            
+            if (int.TryParse(c.ToString(), out var num))
             {
-                numbersFound.Add(i);
+                numbersFound.Add(num);
             }
+            else if (parseWords)
+            {
+                for (var j = 1; j <= words.Count; j++)
+                {
+                    var word = words[j-1];
+                    if(line.Length < i + word.Length) continue;
+                    if (line.Substring(i, word.Length) == word)
+                    {
+                        numbersFound.Add(j);
+                    }
+                }
+            }
+        }
+
+        if (!numbersFound.Any())
+        {
+            return -1;
         }
 
         return int.Parse($"{numbersFound.First()}{numbersFound.Last()}");
--- AdventOfCode2023Tests/Solutions/Day01Tests.cs
+++ AdventOfCode2023Tests/Solutions/Day01Tests.cs
@@ -10,7 +10,7 @@ public class Day01Tests
     public void Example_01_Should_Be_142()
     {
         var expected = 142;
-        var result = Day01.Solve(Day01Inputs.Example01);
+        var result = Day01.Solve(Day01Inputs.Example01, false);
         result.Should().Be(expected);
     }
     
@@ -18,7 +18,23 @@ public class Day01Tests
     public void Real_01_Should_Be_55002()
     {
         var expected = 55002;
-        var result = Day01.Solve(Day01Inputs.Real);
+        var result = Day01.Solve(Day01Inputs.Real, false);
+        result.Should().Be(expected);
+    }
+    
+    [Fact]
+    public void Example_02_Should_Be_281()
+    {
+        var expected = 281;
+        var result = Day01.Solve(Day01Inputs.Example02, true);
+        result.Should().Be(expected);
+    }
+    
+    [Fact]
+    public void Real_02_Should_Be_55093()
+    {
+        var expected = 55093;
+        var result = Day01.Solve(Day01Inputs.Real, true);
         result.Should().Be(expected);
     }
 }
