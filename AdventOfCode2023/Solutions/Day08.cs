39b41b273e215c4bf652143ebf05f36aa1a2119f
Shaun Hevey <shaun.hevey@gmail.com>
2023-12-09 15:58:37 +1100

    Day 08 Part 02

--- AdventOfCode2023/Solutions/Day08.cs
+++ AdventOfCode2023/Solutions/Day08.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Runtime;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode2023.Solutions;
@@ -14,9 +15,113 @@ public static class Day08
 
         var places = lines.Select(ParsePlace).Where(x => x.HasValue).ToList();
         return GetStepCount(steps, places);
-        return -1;
     }
+    
+    public static long SolveGhost(string input)
+    {
+        var lines = input.Split(Environment.NewLine).ToList();
+        var steps = lines.First();
 
+        lines.Remove(steps);
+
+        var places = lines.Select(ParsePlace).Where(x => x.HasValue).ToList();
+        return GetStepCountGhost(steps, places);
+    }
+    
+    private static long GetStepCountGhost(string steps, List<(string id, string left, string right)?> places)
+    {
+        var mapPlaces = new Dictionary<string, (string left, string right)>();
+        foreach (var place in places.Where(x => x.HasValue))
+        {
+            var p = place ?? throw new Exception("Couldn't get place to map");
+            mapPlaces[p.id] = (p.left, p.right);
+        }
+
+        var firstPlaces = mapPlaces.Keys.Where(x => x.EndsWith('A'));
+
+        var cycles = new List<List<long>>();
+        foreach (var firstPlaceId in firstPlaces)
+        {
+            var cycle = new List<long>();
+            var firstPlace = mapPlaces[firstPlaceId];
+            var currentPlace = (firstPlace.left, firstPlace.right);
+            var currentStepCount = 0;
+            var placeIdToGo = "";
+            string? firstZ = null;
+            
+            while (true)
+            {
+                while (currentStepCount == 0 || !placeIdToGo.EndsWith("Z"))
+                {
+                    var currectDirPlace = currentStepCount % steps.Length;
+                    var dir = steps.ToCharArray()[currectDirPlace];
+                    if (dir == 'L')
+                    {
+                        placeIdToGo = currentPlace.left;
+                        currentPlace = mapPlaces[currentPlace.left];
+                    }
+
+                    if (dir == 'R')
+                    {
+                        placeIdToGo = currentPlace.right;
+                        currentPlace = mapPlaces[currentPlace.right];
+                    }
+
+                    currentStepCount++;
+                }
+
+                cycle.Add(currentStepCount);
+
+                if (firstZ == null)
+                {
+                    firstZ = placeIdToGo;
+                    currentStepCount = 0;
+                }
+                else if(firstZ == placeIdToGo)
+                {
+                    break;
+                }
+            }
+            cycles.Add(cycle);
+        }
+
+        var nums = cycles.Select(x => x.First()).ToList();
+
+        var lcm = nums.First();
+        nums.RemoveAt(0);
+
+        foreach (var num in nums)
+        {
+            lcm = FindLCM(lcm, num);
+        }
+        return lcm;
+    }
+    
+    private static long FindLCM(long a, long b)
+    {
+        long num1, num2;
+
+        if (a > b)
+        {
+            num1 = a;
+            num2 = b;
+        }
+        else
+        {
+            num1 = b;
+            num2 = a;
+        }
+
+        for (long i = 1; i <= num2; i++)
+        {
+            if ((num1 * i) % num2 == 0)
+            {
+                return i * num1;
+            }
+        }
+        return num2;
+    }
+    
     private static long GetStepCount(string steps, List<(string id, string left, string right)?> places)
     {
         var mapPlaces = new Dictionary<string, (string left, string right)>();
--- AdventOfCode2023Tests/Solutions/Day08Tests.cs
+++ AdventOfCode2023Tests/Solutions/Day08Tests.cs
@@ -15,6 +15,14 @@ public class Day08Tests
     }
     
     [Fact]
+    public void Example_02_Should_Be_6()
+    {
+        var expected = 6;
+        var result = Day08.SolveGhost(Day08Inputs.Example02);
+        result.Should().Be(expected);
+    }
+    
+    [Fact]
     public void Real_01_Should_Be_20093()
     {
         var expected = 20093;
@@ -22,5 +30,12 @@ public class Day08Tests
         result.Should().Be(expected);
     }
     
+    [Fact]
+    public void Real_02_Should_Be_22103062509257()
+    {
+        var expected = 22103062509257;
+        var result = Day08.SolveGhost(Day08Inputs.Real);
+        result.Should().Be(expected);
+    }
     
 }
